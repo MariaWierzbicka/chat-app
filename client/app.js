2ef8d2036fea86f119936a3fb7f443158bed3398
Maria Wierzbicka <maria.a.wierzbicka@gmail.com>
2022-07-14 19:44:59 +0200

    Add client-server communication

--- client/app.js
+++ client/app.js
@@ -6,22 +6,21 @@ const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 
+const socket = io();
+
 let userName = ('');
 
-const login = () => {
+const login = e => {
+  e.preventDefault();
   if (userNameInput.value === ''){
     window.alert("This field can't be empty!")
   } else {
     userName = userNameInput.value;
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
+    socket.emit('login', userName);
   }
 };
-
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  login();
-});
 
 
 const addMessage = (author, content) => {
@@ -38,16 +37,47 @@ const addMessage = (author, content) => {
   messagesList.appendChild(message);
 };
 
-const sendMessage = () => {
-  if (messageContentInput.value === ''){
+
+const sendMessage = e => {
+  e.preventDefault();
+  if (messageContentInput.value.length < 1){
     window.alert("Message can't be empty!")
   } else {
     addMessage(userName, messageContentInput.value);
+    socket.emit('message', { author: userName, content: messageContentInput.value });
     messageContentInput.value = '';
   }
 };
 
-addMessageForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  sendMessage();
-})
+const joinConversation = newUser => {
+    const botMessageJoined = document.createElement('li');
+  
+    botMessageJoined.classList.add('message');
+    botMessageJoined.classList.add('message--received');
+    
+    botMessageJoined.innerHTML = (`<h3 class="message__author">Chat Bot</h3>
+      <div class="message__content bot-message" >${newUser} has joined the conversation!</div>`);
+    
+    messagesList.appendChild(botMessageJoined);
+};
+const leaveConversation = leavingUser => {
+  const botMessageLeft = document.createElement('li');
+
+  botMessageLeft.classList.add('message');
+  botMessageLeft.classList.add('message--received');
+  
+  botMessageLeft.innerHTML = (`<h3 class="message__author">Chat Bot</h3>
+    <div class="message__content bot-message" >${leavingUser} has left the conversation... :(</div>`);
+  
+  messagesList.appendChild(botMessageLeft);
+};
+
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('botMessageJoined', (newUser) => joinConversation(newUser));
+socket.on('botMessageLeft', (leavingUser) => leaveConversation(leavingUser));
+
+
+loginForm.addEventListener('submit', (e) => login(e));
+addMessageForm.addEventListener('submit', (e) => sendMessage(e));
+
--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 const express = require('express');
 const path = require('path');
-
+const socket = require('socket.io');
 const app = express();
-
 app.use(express.static(path.join(__dirname, '/client')));
 
+let messages = [];
+let users = [];
 
-const messages = [];
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000)
+});
+
+const io = socket(server);
 
 app.get('/', (req, res) => {
   res.render('client/index');
 });
 
+io.on('connection', (socket) => {
+
+  socket.on('login', (name) => {
+    const user = {name, id: socket.id};
+    users.push(user);
+    socket.broadcast.emit('botMessageJoined', name);
+  });
+
+  socket.on('message', (message) => { 
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('disconnect', (name) => {
+    const leavingUser = users.find(user => user.id === socket.id);
+    const userIndex = users.indexOf(leavingUser);
+    users.splice(userIndex, 1);
+    if(users.length < 1){
+      messages = []
+    } else {
+      socket.broadcast.emit('botMessageLeft', leavingUser.name);
+    }
+  });
+});
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
-})
-
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
 });
